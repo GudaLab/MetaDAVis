235cf8f6a23997314cba33d4aee3b372116f6bfa
Guda Lab <118491177+GudaLab@users.noreply.github.com>
2024-02-05 14:16:26 -0600

    Add files via upload

--- ui.R
+++ ui.R
@@ -67,7 +67,7 @@ shinyUI(
         <hr>
         <h3> Launch iMGDAViz using R and GitHub</h3>
         <p> iMGDAViz were deposited under the GitHub repository: <br>
-        Before running the app, the user must have R (>= 4.3.1), RStudio (>= 2023.09.0), Bioconductor (3.18) and Shiny (>= 1.7.5) (Tested with this version).<br>
+        Before running the app, the user must have R (>= 4.3.1), RStudio (>= 2023.09.0), Bioconductor (3.17) and Shiny (>= 1.7.5) (Tested with this version).<br>
          If users use an older R version, they may encounter errors in installing packages, So the users are recommended to update their R version first.<br>
          Once the user opens the R in the command line or Rstudio, need to run the following command in R to install the shiny package.<br><br></p>
           
