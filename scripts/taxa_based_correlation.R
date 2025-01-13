fe7f49adfd1e83fdaed842dfb0d6b3ac9bb4c8a3
Guda Lab <118491177+GudaLab@users.noreply.github.com>
2025-01-13 16:07:08 -0600

    Delete scripts directory

--- scripts/taxa_based_correlation.R
+++
@@ -1,9 +0,0 @@
-library("ggpubr")
-library("GGally")
-taxa_based_correlation_plot_table <- function(OTU_input, group_index, method, labe_size){
-OTU_input <- as.data.frame(t(OTU_input))
-p<-ggcorr(OTU_input, label = FALSE, label_alpha = TRUE, size = labe_size, geom = "circle", method = c("pairwise", method))
-p
-p$data <- p$data %>% mutate(across(where(is.numeric), ~ round(.,2)))
-return(list(plot=p, data=p$data))
-}
